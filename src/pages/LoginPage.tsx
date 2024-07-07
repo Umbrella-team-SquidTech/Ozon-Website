4432882ac877b94ae3633465d96e934365725a64
Zvki1 <rzaki@hotmail.fr>
2024-07-07 22:52:18 +0100

    feat: add shadcn toast

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,18 +2,17 @@ import LoginForm from "@/components/Login/LoginForm";
 import LoginFooter from "@/components/Login/LoginFooter";
 import LoginHeader from "@/components/Login/LoginHeader";
 import RegisterLink from "@/components/Login/RegisterLink";
-
 export default function LoginPage() {
-  
   return (
     <div className="md:flex  overflow-x-hidden h-screen md:h-full justify-center items-center font-Outfit md:py-12 bg-BgColor">
       <div className="h-full md:max-h-auto   border flex flex-col justify-evenly rounded-lg py-10 px-6 md:p-16 gap-7 w-full  md:w-[45%]">
         <LoginHeader />
-        <div><LoginForm />
-        <RegisterLink /></div>
+        <div>
+          <LoginForm />
+          <RegisterLink />
+        </div>
         <LoginFooter />
       </div>
     </div>
-    
   );
 }
