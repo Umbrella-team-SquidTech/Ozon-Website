6ddeb159941580c79491b0ed163f5ac530681566
Zvki1 <rzaki@hotmail.fr>
2024-07-07 18:35:47 +0100

    refactor:fixing the overflow

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,9 +4,9 @@ import LoginHeader from "@/components/Login/LoginHeader";
 export default function LoginPage() {
   return (
     <>
-    <div className="h-screen w-screen  font-Outfit bg-BgColor">
+    <div className="h-screen w-screen overflow-x-hidden flex flex-items-center justify-center  font-Outfit bg-BgColor">
       {/* the flex was deleted and the width was set to screen */}
-      <div className="max-h-auto  w-full border flex flex-col justify-between rounded-lg py-10 px-6 md:p-16 gap-7">
+      <div className="max-h-auto  w-full md:w-[45%] border flex flex-col justify-between rounded-lg py-10 px-6 md:p-16 gap-7">
         <LoginHeader />
         <LoginForm />
         <LoginFooter />
