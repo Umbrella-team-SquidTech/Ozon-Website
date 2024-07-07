3fb50e850430656beb5a7da9db4a60f3c150b48f
Zvki1 <rzaki@hotmail.fr>
2024-07-07 13:28:28 +0100

    feat:stylinig the inputs and labels

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,11 +1,10 @@
 import LoginLink from "@/components/Register/LoginLink"
 import OauthLinks from "@/components/Register/OauthLinks"
-import RegisterForm from "@/components/Register/RegisterForm"
 import RegisterHeader from "@/components/Register/RegisterHeader"
 import {RegisterMobileForm} from "@/components/Register/register-mobile-form"
 const RegisterPage = () => {
   return (
-    <div className="w-screen h-[100dvh] flex flex-col items-center pt-14 ">
+    <div className="w-screen h-[100dvh] flex flex-col items-center pt-14 bg-BgColor">
         <RegisterHeader />
         {/* <RegisterForm /> */}
         <RegisterMobileForm />
