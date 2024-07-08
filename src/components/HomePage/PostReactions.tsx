7e137bd3222b7177cbdc4a32301cc6a04a3933ff
Zvki1 <rzaki@hotmail.fr>
2024-07-08 23:06:51 +0100

    refactor: turning the Reactions icons into buttons

--- src/components/HomePage/PostReactions.tsx
+++ src/components/HomePage/PostReactions.tsx
@@ -9,10 +9,10 @@ const PostReactions = () => {
   return (
     <div className="flex justify-between items-center mt-4">
       <div className="flex items-center space-x-4">
-        <Heart className="w-6 h-6 text-[#2D3A3A]" />
-        <MessageCircle className="w-6 h-6 text-[#2D3A3A]" />
-        <Repeat2 className="w-6 h-6 text-[#2D3A3A]" />
-        <SquareArrowOutUpRight className="w-6 h-6 text-[#2D3A3A]" />
+        <button><Heart className="w-6 h-6 text-[#2D3A3A]" /></button>
+        <button><MessageCircle className="w-6 h-6 text-[#2D3A3A]" /></button>
+        <button><Repeat2 className="w-6 h-6 text-[#2D3A3A]" /></button>
+        <button><SquareArrowOutUpRight className="w-6 h-6 text-[#2D3A3A]" /></button>
       </div>    
     </div>
   );
