d01a32020be92a2dffcac3e03fa8582284b83890
Zvki1 <rzaki@hotmail.fr>
2024-07-08 01:06:35 +0100

    refactor:add hide/view password in the register desktop version

--- src/components/Register/RegisterDeskForm.tsx
+++ src/components/Register/RegisterDeskForm.tsx
@@ -11,8 +11,8 @@ import {
 import formSchema from "@/schemas/RegisterSchema";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-
-// TODO: add hide password
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
 export default function RegisterDeskForm() {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -30,6 +30,8 @@ export default function RegisterDeskForm() {
     // REQUEST API
     console.log(values);
   }
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -93,12 +95,25 @@ export default function RegisterDeskForm() {
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
-                        <Input
-                          type="password"
-                          className=" h-14"
-                          placeholder="Password"
-                          {...field}
-                        />
+                        <div className="relative">
+                          <Input
+                            type={showPassword ? "text" : "password"}
+                            className=" h-14"
+                            placeholder="Mot de passe"
+                            {...field}
+                          />
+                          <button
+                            type="button"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="absolute inset-y-0 end-2"
+                          >
+                            {!showPassword ? (
+                              <Eye color="#2D3A3A" />
+                            ) : (
+                              <EyeOff color="#2D3A3A" />
+                            )}
+                          </button>
+                        </div>
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -112,12 +127,25 @@ export default function RegisterDeskForm() {
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
+                        <div className="relative">
                         <Input
-                          type="password"
+                          type={showConfirmPassword ? "text" : "password"}
                           className=" h-14"
                           placeholder="Confirmer le mot de passe"
                           {...field}
                         />
+                         <button
+                         type="button"
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                className="absolute inset-y-0 end-2"
+              >
+                {!showConfirmPassword ? (
+                  <Eye color="#2D3A3A" />
+                ) : (
+                  <EyeOff color="#2D3A3A" />
+                )}
+              </button>
+                        </div>
                       </FormControl>
                       <FormMessage />
                     </FormItem>
