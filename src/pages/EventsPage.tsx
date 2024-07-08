7d7fed585fb6cd0b34693be160ced9de86a13323
Zvki1 <rzaki@hotmail.fr>
2024-07-08 23:12:26 +0100

    feat:create eventPage and add it to the router

---
+++ src/pages/EventsPage.tsx
@@ -0,0 +1,14 @@
+import RootLayout from "@/components/RootLayout";
+
+
+const Events = () => {
+  return (
+    <RootLayout>
+      <div className="p-5 pt-0 md:px-20">
+        <h1>Events</h1>
+      </div>
+    </RootLayout>
+  )
+}
+
+export default Events
