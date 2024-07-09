0146c268e82a2bec9960f2197835700e00f4c89d
Younesfdj <ferradjiyounes69@gmail.com>
2024-07-10 00:34:15 +0100

    refactor: add responsive home page skeleton

--- src/components/Skeletons/PostSkeleton.tsx
+++ src/components/Skeletons/PostSkeleton.tsx
@@ -2,18 +2,18 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function PostSkeleton() {
   return (
-    <div className="flex h-auto w-[80%] mx-auto">
+    <div className="flex h-auto md:w-[80%] w-full md:mx-auto gap-2 md:gap-0">
       <div className="w-16">
         <Skeleton className="w-12 h-12 rounded-full" />
       </div>
       <div className="w-full pt-2 flex flex-col gap-8">
         <div className="flex flex-col gap-2">
-          <Skeleton className="w-[9rem] h-4" />
-          <Skeleton className="w-[5rem] h-4" />
+          <Skeleton className="w-[9rem] h-3 md:h-4" />
+          <Skeleton className="w-[5rem] h-3 md:h-4" />
         </div>
         <div className="flex flex-col gap-2">
-          <Skeleton className="w-[35rem] h-4" />
-          <Skeleton className="w-[23rem] h-4" />
+          <Skeleton className="w-[17rem] md:w-[35rem] h-3 md:h-4" />
+          <Skeleton className="w-[13rem] md:w-[23rem] h-3 md:h-4" />
         </div>
         <Skeleton className="w-[14rem] h-[14rem]" />
       </div>
--- src/components/Skeletons/SuggestEventSkeleton.tsx
+++ src/components/Skeletons/SuggestEventSkeleton.tsx
@@ -3,14 +3,14 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function SuggestEventSkeleton() {
   return (
     <div className="mt-4 w-full flex">
-      <div className="w-full p-4 md:p-7 flex justify-around">
+      <div className="w-full p-4 md:p-7 flex justify-around gap-5 md:gap-0">
         <div className=" flex flex-col gap-4">
-          <Skeleton className="w-[15rem] h-4" />
-          <Skeleton className="w-[20rem] h-4" />
-          <Skeleton className="w-[15rem] h-3 mt-3" />
+          <Skeleton className="w-[6rem] md:w-[15rem] h-3 md:h-4" />
+          <Skeleton className="w-[11rem] md:w-[20rem] h-3 md:h-4" />
+          <Skeleton className="w-[6rem] md:w-[15rem] h-2 md:h-3 mt-3" />
         </div>
         <div className="flex flex-row gap-1 items-center ">
-          <Skeleton className="w-[30rem] h-28" />
+          <Skeleton className="w-[8rem] md:w-[30rem] h-28" />
         </div>
       </div>
     </div>
