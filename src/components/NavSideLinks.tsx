ffa88213ee985cdd1c5e5206e3b087d2094adac1
Younesfdj <ferradjiyounes69@gmail.com>
2024-07-09 17:30:00 +0100

    feat: add profile pop over

--- src/components/NavSideLinks.tsx
+++ src/components/NavSideLinks.tsx
@@ -1,6 +1,36 @@
 import { Search, Bell, MessageCircleMore } from "lucide-react";
 import UserProfile from "@/assets/UserProfile.png";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { MapPin } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import axios from "@/config/axios";
+import useUserStore from "@/stores/useUser";
+import { useToast } from "./ui/use-toast";
+
 export default function NavSideLinks() {
+  const { removeUser } = useUserStore();
+  const navigate = useNavigate();
+  const { toast } = useToast();
+  function handleLogout() {
+    axios
+      .post("/logout")
+      .then(() => {
+        removeUser();
+        navigate("/login");
+      })
+      .catch(() => {
+        toast({
+          title: "Erreur",
+          description: "Une erreur s'est produite lors de la déconnexion",
+          variant: "destructive",
+        });
+      });
+  }
   return (
     <div className="flex md:gap-3">
       <div className="bg-gray-200 rounded-3xl w-12 h-11 md:flex justify-center items-center cursor-pointer hidden">
@@ -12,10 +42,41 @@ export default function NavSideLinks() {
       <div className=" rounded-3xl w-12 h-11 flex justify-center items-center cursor-pointer md:hidden">
         <MessageCircleMore />
       </div>
-      <img
-        src={UserProfile}
-        className=" rounded-3xl w-12 h-11 md:flex justify-center items-center cursor-pointer hidden "
-      />
+      <Popover>
+        <PopoverTrigger>
+          <img
+            src={UserProfile}
+            className=" rounded-3xl w-12 h-11 md:flex justify-center items-center cursor-pointer hidden "
+          />
+        </PopoverTrigger>
+        <PopoverContent className=" min-w-56 min-h-56 p-7 flex flex-col justify-between">
+          <div className="flex gap-2 w-full">
+            <img
+              src={UserProfile}
+              className=" rounded-3xl w-16 h-14 md:flex justify-center items-center cursor-pointer hidden "
+            />
+            <div className=" w-[20rem] flex flex-col gap-1">
+              <h2 className="w-full font-[600]">Karim Younes</h2>
+              <p className="text-[12px] flex gap-1 items-center">
+                <MapPin className="w-4 h-4" />
+                Paris, France
+              </p>
+              <p className="text-[10px]">Membre depuis 7 mai 2024</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Link to="/profile" className="hover:bg-gray-200 p-2 rounded-lg">
+              Profile
+            </Link>
+            <button
+              className="bg-transparent text-start text-black hover:bg-gray-200 p-2 rounded-lg"
+              onClick={handleLogout}
+            >
+              Se déconnecter
+            </button>
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
