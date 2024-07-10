7754ea6087e238b5f8a1929f7aa8c129800f1263
Younesfdj <ferradjiyounes69@gmail.com>
2024-07-10 19:49:12 +0100

    feat: integrate register user

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -8,8 +8,8 @@ import LoginFooter from "@/components/Login/LoginFooter";
 const RegisterPage = () => {
   return (
     <div
-      className="w-screen h-[100dvh] overflow-x-hidden flex flex-col items-center justify-evenly
-    md:justify-start py-10  md:py-0 md:pt-14 bg-BgColor font-Outfit"
+      className="w-screen h-screen overflow-x-hidden flex flex-col items-center overflow-y-auto justify-evenly
+    md:justify-start py-5 md:py-0 md:pt-14 bg-BgColor font-Outfit"
     >
       <div className="max-h-auto  border hidden md:flex flex-col justify-between rounded-lg p-16 gap-7 w-[45%]">
         <LoginHeader register={true} />
@@ -17,7 +17,7 @@ const RegisterPage = () => {
         <LoginFooter />
       </div>
 
-      <div className=" md:hidden px-6">
+      <div className=" md:hidden px-6 ">
         <RegisterHeader />
         <div>
           <RegisterMobileForm />
