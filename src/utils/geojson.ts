7a5ba2a67b328e40bd8ed492612e3ba4ea45373d
Zvki1 <rzaki@hotmail.fr>
2024-07-10 20:00:18 +0100

    refactor:evetsMap integration

--- src/utils/geojson.ts
+++ src/utils/geojson.ts
@@ -1,32 +1,25 @@
-const template = {
-  features: [
-    {
-      type: "Feature",
-      properties: {
-        title: "Lincoln Park",
-        description: "A northside park that is home to the Lincoln Park Zoo",
-      },
-      geometry: {
-        coordinates: [-87.637596, 41.940403],
-        type: "Point",
-      },
-    },
-    {
-      type: "Feature",
-      properties: {
-        title: "Burnham Park",
-        description: "A lakefront park on Chicago's south side",
-      },
-      geometry: {
-        coordinates: [-87.603735, 41.829985],
-        type: "Point",
-      },
-    },
-  ],
-  type: "FeatureCollection",
-};
 
-
-export function generateFeatures(events: string[]) {
-  return
+export function generateFeatures(events: EventI[]) {
+  
+  return {
+    type: "FeatureCollection",
+    features: events.map(event => ({
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [event.longitude, event.latitude]
+      },
+      properties: {
+        id: event.id,
+        name: event.name,
+        description: event.description,
+        address: event.address,
+        start: event.start,
+        organizer: event.organizer,
+        event_type: event.event_type,
+        images: event.images,
+        participations: event.participations
+      }
+    }))
+  };
 }
