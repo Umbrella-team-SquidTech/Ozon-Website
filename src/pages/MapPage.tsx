aacdb79469487ffb71b4bfebb5a6d6003a6e43fa
Zvki1 <rzaki@hotmail.fr>
2024-07-10 16:24:24 +0100

    refactor:change markers path

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -2,7 +2,7 @@
 import "../index.css";
 import { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
-import markers from "../markers.json";
+import markers from "../data/markers.json";
 import RootLayout from "@/components/RootLayout";
 import { createRoot } from "react-dom/client";
 import { Feature } from "geojson";
@@ -12,26 +12,22 @@ import axios from "axios";
 import userMarkerImg from "../assets/MapPage/Userpin.svg";
 
 function MapPage() {
- 
-  mapboxgl.accessToken = "pk.eyJ1IjoienZraTEiLCJhIjoiY2x5ZWNhZXRpMDExcDJrcW8zaTlubDFlMSJ9.JKxu32laTXbJs1hmX0-1VA";
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoienZraTEiLCJhIjoiY2x5ZWNhZXRpMDExcDJrcW8zaTlubDFlMSJ9.JKxu32laTXbJs1hmX0-1VA";
 
-  
   const mapContainer = useRef(null);
   const ref = useRef<null | HTMLDivElement>(null);
   const map = useRef<mapboxgl.Map | null>(null);
-
 
   const [longOfUSer, setLng] = useState(0);
   const [lattOfUser, setLat] = useState(0);
   const [zoom] = useState(9);
 
-
   const userMarker = useRef<mapboxgl.Marker | null>(null);
 
-  
   useEffect(() => {
-   
-    axios.get("http://ip-api.com/json/")
+    axios
+      .get("http://ip-api.com/json/")
       .then((response) => {
         // console.log(response.data);
         setLng(response.data.lon);
@@ -43,7 +39,6 @@ function MapPage() {
 
     if (map.current) return;
 
-    
     map.current = new mapboxgl.Map({
       container: mapContainer.current || document.createElement("div"),
       style: "mapbox://styles/mapbox/streets-v12",
@@ -69,20 +64,17 @@ function MapPage() {
 
     map.current.addControl(new mapboxgl.NavigationControl());
 
-
     const userMarkerElement = document.createElement("div");
-   
+
     userMarkerElement.style.backgroundImage = `url(${userMarkerImg})`;
     userMarkerElement.style.width = "28px";
     userMarkerElement.style.height = "44px";
     userMarkerElement.style.backgroundSize = "cover";
 
-    
     userMarker.current = new mapboxgl.Marker(userMarkerElement)
       .setLngLat([longOfUSer, lattOfUser])
       .addTo(map.current);
   }, []);
-
 
   useEffect(() => {
     if (map.current) {
