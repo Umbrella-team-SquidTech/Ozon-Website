c0c1515086570382b39955cba20c0f4b71bc09a6
Younesfdj <ferradjiyounes69@gmail.com>
2024-07-14 00:24:23 +0100

    refactor: update home page placeholder

--- src/components/HomePage/HomePagePlaceholder.tsx
+++ src/components/HomePage/HomePagePlaceholder.tsx
@@ -2,8 +2,9 @@ import SuggestEventSkeleton from "@/components/Skeletons/SuggestEventSkeleton";
 import PostSkeleton from "@/components/Skeletons/PostSkeleton";
 export default function HomePagePlaceholder() {
   return (
-    <div className="p-5 pt-0 md:px-20 flex flex-col gap-4">
+    <div className="p-5 pt-0 md:px-20 h-auto flex flex-col gap-4 pb-[75px]">
       <SuggestEventSkeleton />
+      <PostSkeleton />
       <PostSkeleton />
     </div>
   );
